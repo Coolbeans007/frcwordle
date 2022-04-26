88b087622e2435d9114e09687ac364b314ed493e
Coolbeans007 <coolbeans007@outlook.com>
2022-04-25 22:23:39 -0500

    deploy

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,7 +2,7 @@ export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough numbers'
 export const WORD_NOT_FOUND_MESSAGE = 'Team not found'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
