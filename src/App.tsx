99217d2ce73e8f455664e4f08457c0e07462ae91
Coolbeans007 <coolbeans007@outlook.com>
2022-04-25 19:12:31 -0500

    spacing done

--- src/App.tsx
+++ src/App.tsx
@@ -258,7 +258,7 @@ function App() {
         setIsSettingsModalOpen={setIsSettingsModalOpen}
       />
       <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
-        <div className="pb-6 grow">
+        <div className="pb-6 shrink">
           <Grid
             solution={solution}
             guesses={guesses}
